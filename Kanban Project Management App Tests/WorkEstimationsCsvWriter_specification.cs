41684a56a937288bf4f6c899a791326e587faa62
xilconic <xilconic@gmail.com>
2020-09-02 19:58:50 +0200

    Implement csv writer class for a collection of WorkEstimates

---
+++ Kanban Project Management App Tests/WorkEstimationsCsvWriter_specification.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) 2020 S.L. des Bouvrie
+//
+// This file is part of 'Kanban Project Management App'.
+//
+// Kanban Project Management App is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Kanban Project Management App is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Kanban Project Management App.  If not, see https://www.gnu.org/licenses/.
+using KanbanProjectManagementApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace KanbanProjectManagementApp.Tests
+{
+    public class WorkEstimationsCsvWriter_specification
+    {
+        [Fact]
+        public void WHEN_constructing_writer_AND_text_writer_is_null_THEN_throw_ArgumentNullException()
+        {
+            void call() => new WorkEstimationsCsvWriter(null);
+            var actualException = Assert.Throws<ArgumentNullException>(call);
+            Assert.Equal("textWriter", actualException.ParamName);
+        }
+
+        public static IEnumerable<object[]> WriterScenarios
+        {
+            get
+            {
+                yield return new object[]
+                {
+                    Array.Empty<WorkEstimate>(),
+@"Number of days till completion in simulation;Is indeterminate
+"
+                };
+
+                yield return new object[]
+                {
+                    new[] { new WorkEstimate(1.1, true) },
+@"Number of days till completion in simulation;Is indeterminate
+1.1;True
+"
+                };
+
+                yield return new object[]
+                {
+                    new[] { new WorkEstimate(2.2, true), new WorkEstimate(3.3, false),  },
+@"Number of days till completion in simulation;Is indeterminate
+2.2;True
+3.3;False
+"
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(WriterScenarios))]
+        public void GIVEN_empty_work_estimations_WHEN_writing_THEN_only_header_has_been_written(
+            IReadOnlyCollection<WorkEstimate> workEstimates,
+            string expectedOutput)
+        {
+            using (var stringWriter = new StringWriter())
+            {
+                var workEstimationsCsvWriter = new WorkEstimationsCsvWriter(stringWriter);
+                workEstimationsCsvWriter.Write(workEstimates);
+
+                var output = stringWriter.ToString();
+                Assert.Equal(expectedOutput, output);
+            }
+        }
+    }
+}
---
+++ Kanban Project Management App/WorkEstimationsCsvWriter.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) 2020 S.L. des Bouvrie
+//
+// This file is part of 'Kanban Project Management App'.
+//
+// Kanban Project Management App is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Kanban Project Management App is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Kanban Project Management App.  If not, see https://www.gnu.org/licenses/.
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+using KanbanProjectManagementApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace KanbanProjectManagementApp
+{
+    internal class WorkEstimationsCsvWriter
+    {
+        private readonly TextWriter textWriter;
+
+        public WorkEstimationsCsvWriter(TextWriter textWriter)
+        {
+            this.textWriter = textWriter ?? throw new ArgumentNullException(nameof(textWriter));
+        }
+
+        public void Write(IReadOnlyCollection<WorkEstimate> workEstimates)
+        {
+            var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ";",
+                SanitizeForInjection = true,
+            };
+            using(var csvWriter = new CsvWriter(textWriter, configuration, true))
+            {
+                csvWriter.WriteRecords(workEstimates.Select(WorkEstimateRow.FromDomain));
+            }
+        }
+
+        private class WorkEstimateRow
+        {
+            [Index(0)]
+            [Name("Number of days till completion in simulation")]
+            public double EstimatedNumberOfWorkingDaysRequired { get; set; }
+
+            [Index(1)]
+            [Name("Is indeterminate")]
+            public bool IsIndeterminate { get; set; }
+
+            public static WorkEstimateRow FromDomain(WorkEstimate we) =>
+                new WorkEstimateRow
+                {
+                    EstimatedNumberOfWorkingDaysRequired = we.EstimatedNumberOfWorkingDaysRequired,
+                    IsIndeterminate = we.IsIndeterminate,
+                };
+        }
+    }
+}
