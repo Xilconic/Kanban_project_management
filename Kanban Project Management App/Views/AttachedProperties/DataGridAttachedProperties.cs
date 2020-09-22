3b682ec408da778ca467571b5326732106914e9b
xilconic <xilconic@gmail.com>
2020-09-22 20:20:18 +0200

    Define attached property to allow dynamic DataGrid.Columns
    Note: When a ObservableCollection<T> gets cleared, it emits an reset collection changed event where the new set of columns is null. Therefore an null-check is needed to see if there are actually columns provided during the reset.

---
+++ Kanban Project Management App/Views/AttachedProperties/DataGridAttachedProperties.cs
@@ -0,0 +1,142 @@
+﻿// Copyright (c) 2020 S.L. des Bouvrie
+//
+// This file is part of 'Kanban Project Management App'.
+//
+// Kanban Project Management App is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Kanban Project Management App is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Kanban Project Management App.  If not, see https://www.gnu.org/licenses/.
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KanbanProjectManagementApp.Views.AttachedProperties
+{
+    internal class DataGridAttachedProperties
+    {
+        private const string DynamicColumnsPropertyName = "DynamicColumns";
+        private static readonly Dictionary<DataGrid, NotifyCollectionChangedEventHandler> dynamicColumnCollectionChangedEventHandlersPerDataGrid =
+            new Dictionary<DataGrid, NotifyCollectionChangedEventHandler>();
+
+        public static readonly DependencyProperty DynamicColumnsProperty =
+            DependencyProperty.RegisterAttached(
+                DynamicColumnsPropertyName,
+                typeof(ObservableCollection<DataGridColumn>),
+                typeof(DataGridAttachedProperties),
+                new UIPropertyMetadata(null, DynamicColumnsPropertyChanged));
+
+        [AttachedPropertyBrowsableForTypeAttribute(typeof(DataGrid))]
+        public static ObservableCollection<DataGridColumn> GetDynamicColumns(UIElement target) =>
+            (ObservableCollection<DataGridColumn>)target.GetValue(DynamicColumnsProperty);
+
+        [AttachedPropertyBrowsableForTypeAttribute(typeof(DataGrid))]
+        public static void SetDynamicColumns(UIElement target, ObservableCollection<DataGridColumn> newValue) =>
+            target.SetValue(DynamicColumnsProperty, newValue);
+
+        private static void DynamicColumnsPropertyChanged(DependencyObject source, DependencyPropertyChangedEventArgs e)
+        {
+            var dataGrid = source as DataGrid;
+            if (dataGrid is null)
+            {
+                throw new InvalidOperationException($"Property should be attached to instance of type {nameof(DataGrid)}.");
+            }
+
+            ClearDataGridColumns(dataGrid);
+
+            UnsubscribeFromOldValue(dataGrid, e.OldValue);
+
+            var newDataGridColumns = e.NewValue as ObservableCollection<DataGridColumn>;
+            if (newDataGridColumns is null)
+            {
+                return;
+            }
+
+            AddColumns(dataGrid, newDataGridColumns);
+            SubscribeToNewValue(dataGrid, newDataGridColumns);
+        }
+
+        private static void SubscribeToNewValue(DataGrid dataGrid, ObservableCollection<DataGridColumn> newDataGridColumns)
+        {
+            NotifyCollectionChangedEventHandler eventHandler = (_, collectionChangedEvent) =>
+                NewDataGridColumns_CollectionChanged(dataGrid, collectionChangedEvent);
+            dynamicColumnCollectionChangedEventHandlersPerDataGrid[dataGrid] = eventHandler;
+            newDataGridColumns.CollectionChanged += eventHandler;
+        }
+
+        private static void UnsubscribeFromOldValue(DataGrid dataGrid, object oldValue)
+        {
+            var originalDataGridColumns = oldValue as ObservableCollection<DataGridColumn>;
+            if (originalDataGridColumns != null && dynamicColumnCollectionChangedEventHandlersPerDataGrid.TryGetValue(dataGrid, out var h))
+            {
+                originalDataGridColumns.CollectionChanged -= h;
+                dynamicColumnCollectionChangedEventHandlersPerDataGrid.Remove(dataGrid);
+            }
+        }
+
+        private static void NewDataGridColumns_CollectionChanged(DataGrid dataGridToUpdate, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Reset:
+                    ResetColumns(dataGridToUpdate, e.NewItems);
+                    break;
+                case NotifyCollectionChangedAction.Add:
+                    AddColumns(dataGridToUpdate, e.NewItems);
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    RemoveColumns(dataGridToUpdate, e.OldItems);
+                    break;
+                case NotifyCollectionChangedAction.Move:
+                    dataGridToUpdate.Columns.Move(e.OldStartingIndex, e.NewStartingIndex);
+                    break;
+                case NotifyCollectionChangedAction.Replace:
+                    dataGridToUpdate.Columns[e.NewStartingIndex] = (DataGridColumn)e.NewItems[0];
+                    break;
+                default: throw new InvalidEnumArgumentException("e.Action", (int)e.Action, typeof(NotifyCollectionChangedAction));
+            }
+        }
+
+        private static void ResetColumns(DataGrid dataGridToUpdate, IEnumerable columns)
+        {
+            ClearDataGridColumns(dataGridToUpdate);
+            if(columns != null)
+            {
+                AddColumns(dataGridToUpdate, columns);
+            }
+        }
+
+        private static void ClearDataGridColumns(DataGrid dataGridToUpdate)
+        {
+            dataGridToUpdate.Columns.Clear();
+        }
+
+        private static void AddColumns(DataGrid dataGridToUpdate, IEnumerable columns)
+        {
+            foreach (DataGridColumn c in columns)
+            {
+                dataGridToUpdate.Columns.Add(c);
+            }
+        }
+
+        private static void RemoveColumns(DataGrid dataGridToUpdate, IEnumerable columns)
+        {
+            foreach (DataGridColumn c in columns)
+            {
+                dataGridToUpdate.Columns.Remove(c);
+            }
+        }
+    }
+}
